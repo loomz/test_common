7c14e94af3c8c266dfe031c8fd5aaeba567c5f51
loomz@163.com <loomz@163.com>
2022-10-31 18:45:40 +0800

    优化避免重复计算

--- config.py
+++ config.py
@@ -1,17 +1,24 @@
 import sys
 import getopt
 
+current_env = None
+
 
 def get_env():
+    global current_env
+
+    if current_env:
+        return current_env
+
+    print("sys.argv=%s" % sys.argv)
     opts, args = getopt.getopt(sys.argv[1:], "he:", ["help", "env="])
 
-    env = None
     for opt, arg in opts:
         if opt in ("-h", "--help"):
             print('main.py -e <env>')
             print('or: test_arg.py --env=<env>')
             sys.exit()
         elif opt in ("-e", "--env"):
-            env = arg
+            current_env = arg
 
-    return env
+    return current_env
